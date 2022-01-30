c68916d77b5e6eb92e22dd04726bfada56f7f2d2
Julián Nicolás Spinelli <julian.spinelli.so@softtekosde.com>
2022-01-30 20:27:28 -0300

    cambiado server a heroku

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
-export const SERVER = "http://localhost:8080/api"
-//export const SERVER = "https://po-manager-back.herokuapp.com/api"
+//export const SERVER = "http://localhost:8080/api"
+export const SERVER = "https://po-manager-back.herokuapp.com/api"
